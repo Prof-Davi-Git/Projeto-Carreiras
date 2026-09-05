889cd998acd03a4d00c123b32e7b47439699ebbd
Prof-Davi-Git <davigodoi@prof.educacao.sp.gov.br>
2026-09-05 13:36:52 -0300

    Configura projeto Firebase

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CARREIRAS_CONFIG = Object.freeze({
+  apiKey: "AIzaSyDpOgbLJGl30_c9xXSicZtRAQZqbYAu5rY",
+  authDomain: "meu-futuro-profissional.firebaseapp.com",
+  projectId: "meu-futuro-profissional",
+  storageBucket: "meu-futuro-profissional.firebasestorage.app",
+  messagingSenderId: "375767515039",
+  appId: "1:375767515039:web:86ade72d45ecceec7dc7aa",
+  measurementId: "G-CMR1YVSVDJ"
+});
