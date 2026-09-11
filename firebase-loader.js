044366d3eda50ce30066babc2c5b55624ca6dedc
Prof-Davi-Git <davigodoi@prof.educacao.sp.gov.br>
2026-09-11 08:35:10 -0300

    Atualiza cache da interface de avaliacoes

--- firebase-loader.js
+++ firebase-loader.js
@@ -33,7 +33,7 @@
 
   async function iniciar() {
     try {
-      carregarEstilo(local("firebase-ui.css?v=20260905-1"));
+      carregarEstilo(local("firebase-ui.css?v=20260911-1"));
       await carregar("https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js");
       await carregar("https://www.gstatic.com/firebasejs/10.14.1/firebase-auth-compat.js");
       await carregar("https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore-compat.js");
@@ -51,7 +51,8 @@
       };
 
       if (scripts[pagina]) {
-        await carregar(local(`${scripts[pagina]}?v=20260905-1`));
+        const versao = pagina === "entrevistas.html" ? "20260911-1" : "20260905-1";
+        await carregar(local(`${scripts[pagina]}?v=${versao}`));
       }
     } catch (erro) {
       console.error("Firebase não pôde ser iniciado:", erro);
